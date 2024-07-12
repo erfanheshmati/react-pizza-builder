cf0e2b7fdd38048a10c7db58848c0964632951b8
Erfan Heshmati <heshmati74@gmail.com>
2024-07-12 19:17:07 +0330

    header ui design

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <div className="py-10 px-50 bg-black">
+            <div className="text-white text-center text-2xl font-semibold">
+                Pizza Customization
+            </div>
+        </div>
+    )
+}
